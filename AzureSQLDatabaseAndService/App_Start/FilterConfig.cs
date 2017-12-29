b648e36136745d1af5ba5a90ab88c70088a91f14
gauravanandwhiz <gauravanandwhiz@gmail.com>
2017-12-29 17:38:40 +0530

    Added method functionality Comment

--- AzureSQLDatabaseAndService/App_Start/FilterConfig.cs
+++ AzureSQLDatabaseAndService/App_Start/FilterConfig.cs
@@ -5,6 +5,10 @@ namespace AzureSQLDatabaseAndService
 {
     public class FilterConfig
     {
+        /// <summary>
+        /// The method is used to register filters
+        /// </summary>
+        /// <param name="filters"></param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute()); // Added Comment#1
