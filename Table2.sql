0b419a6ce06fcd012b70923afd5f88f5ef71903b
gauravanandwhiz <gauravanandwhiz@gmail.com>
2018-01-10 14:11:55 +0530

    Commiting Table2

---
+++ Table2.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  File created - Wednesday-January-10-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table TEST_GITHUB
+--------------------------------------------------------
+
+  CREATE TABLE "GAURAV"."TEST_GITHUB" 
+   (	"COLUMN1" VARCHAR2(20 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
