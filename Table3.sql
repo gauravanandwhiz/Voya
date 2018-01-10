7dcd816bd2cb6febfdf2341132024eeae1b111e1
gauravanandwhiz <gauravanandwhiz@gmail.com>
2018-01-10 16:35:43 +0530

    added

---
+++ Table3.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  File created - Wednesday-January-10-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table TABLE3
+--------------------------------------------------------
+
+  CREATE TABLE "GAURAV"."TABLE3" 
+   (	"NAME" VARCHAR2(40 BYTE)
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "USERS" ;
