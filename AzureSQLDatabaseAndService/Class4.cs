e1da22cc35c05886db98fa9ba20c305b48898cfb
gauravanandwhiz <gauravanandwhiz@gmail.com>
2018-01-12 12:17:30 +0530

    commit
    comit

---
+++ AzureSQLDatabaseAndService/Class4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureSQLDatabaseAndService
+{
+    public class Class4
+    {
+        //Class 4 added
+    }
+}
