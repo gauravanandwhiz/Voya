d88cc78983176546c310b67591ae061de2f25098
gauravanandwhiz <gauravanandwhiz@gmail.com>
2018-01-12 12:23:43 +0530

    class 5
    class5

---
+++ AzureSQLDatabaseAndService/App_Start/Class5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureSQLDatabaseAndService.App_Start
+{
+    public class Class5
+    {
+        //Added Class 5
+    }
+}
